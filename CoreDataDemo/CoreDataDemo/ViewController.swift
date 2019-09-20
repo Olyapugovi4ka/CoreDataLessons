aa4b14be89e6096a62c480779d104203d45439f8
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-09-20 15:40:55 +0300

    Create new app for studying CoreData

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by MacBook on 20/09/2019.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
