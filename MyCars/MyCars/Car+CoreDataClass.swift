94e46da72d65a2edc497321119b4944035b8db52
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-09-22 17:15:55 +0300

    saving rating and numbers of engine starting

---
+++ MyCars/MyCars/Car+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Car+CoreDataClass.swift
+//  MyCars
+//
+//  Created by MacBook on 21/09/2019.
+//  Copyright © 2019 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Car)
+public class Car: NSManagedObject {
+
+}
