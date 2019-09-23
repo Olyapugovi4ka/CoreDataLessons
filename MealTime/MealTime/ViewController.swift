52013e3fd386b720ffbe63551a57a40c51bd3b4c
Olga Dakhel <olyapugovi4ka@gmail.com>
2019-09-23 22:00:22 +0300

    Finished course about CoreData

--- MealTime/MealTime/ViewController.swift
+++ MealTime/MealTime/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
+    
+    var context: NSManagedObjectContext!
   
   @IBOutlet weak var tableView: UITableView!
   var array = [Date]()
+    var person : Person!
+    
   
   lazy var dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
@@ -25,6 +30,24 @@ class ViewController: UIViewController, UITableViewDataSource {
     // Do any additional setup after loading the view, typically from a nib.
     
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    
+    let personName = "Max"
+    let fetchRequest: NSFetchRequest<Person> = Person.fetchRequest()
+    fetchRequest.predicate = NSPredicate(format: "name == %@", personName)
+    
+    do{
+        let results = try context.fetch(fetchRequest)
+        if results.isEmpty {
+            person = Person(context: context)
+            person.name = personName
+            try context.save()
+        } else {
+            person = results.first
+        }
+        
+    }catch let error as NSError {
+        print(error.userInfo)
+    }
   }
   
   override func didReceiveMemoryWarning() {
@@ -37,23 +60,55 @@ class ViewController: UIViewController, UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return array.count
-  }
+    
+    guard let meals = person.meals else { return 1 }
+    return meals.count
+    }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
     
-    let date = array[indexPath.row]
+   guard let meal = person.meals?[indexPath.row] as? Meal,
+    let mealDate = meal.date as? Date else {
+        return cell!
+    }
     
-    cell!.textLabel!.text = dateFormatter.string(from: date)
+    cell!.textLabel!.text = dateFormatter.string(from: mealDate)
     return cell!
   }
   
   @IBAction func addButtonPressed(_ sender: AnyObject) {
-    let date = Date()
-    array.append(date)
+   let meal = Meal(context: context)
+    meal.date = NSDate()
+    
+    let meals = person.meals?.mutableCopy() as? NSMutableOrderedSet
+    meals?.add(meal)
+    person.meals = meals
+    
+    do {
+        try context.save()
+    } catch let error as NSError{
+        print("Error: \(error), userinfo \(error.userInfo)")
+    }
     tableView.reloadData()
   }
-  
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard let mealToDelete = person.meals?[indexPath.row] as? Meal, editingStyle == .delete else {
+            return
+        }
+        context.delete(mealToDelete)
+        
+        do {
+            try context.save()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } catch let error as NSError {
+            print("Error: \(error), description \(error.userInfo)")
+        }
+    }
 }
 
